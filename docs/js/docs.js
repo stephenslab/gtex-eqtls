236762b9cc339383dd5819e51df5aa02b411a5e2
Gao Wang <gaow@uchicago.edu>
2017-08-27 18:29:00 -0500

    Build website via jnbinder

---
+++ docs/js/docs.js
@@ -0,0 +1,4 @@
+var analysisArray = ['20170518_Imputation', '20170515_RNASeq_Analysis', '20170424_dbGaP_Download']
+var analysisDict = {"Download-data-from-dbGaP-1":"20170424_dbGaP_Download","RNA-Seq-data-preprocessing-1":"20170515_RNASeq_Analysis","GTEx-V7-genotype-data-imputation-1":"20170518_Imputation"}
+var writeupArray = ['GTEx7_Analysis_Plan']
+var writeupDict = {"GTEx-V7-eQTL-data-analysis-procedure-1":"GTEx7_Analysis_Plan"}
